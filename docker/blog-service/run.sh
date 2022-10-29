8351cfe1d7cf34750e04e2a684f3b064bb4c0b57
AwIlL <awillcezo@gmail.com>
2022-10-29 22:03:10 +0200

    generate uncompressed css

--- docker/blog-service/run.sh
+++ docker/blog-service/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sass -s compressed --no-source-map --verbose -w blog/src/sass/style.scss blog/static/style.css &
+# sass -s compressed --no-source-map --verbose -w blog/src/sass/style.scss blog/static/style.css &
+sass --no-source-map --verbose -w blog/src/sass/style.scss blog/static/style.css &
 sh /run-service.sh &
 
 wait
