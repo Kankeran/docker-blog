f412421bb0eb310204411226329c7d583d5f5150
AwIlL <awillcezo@gmail.com>
2022-10-29 18:21:28 +0200

    handle in docker changes of the project structure

---
+++ docker/blog-service/run-service.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while true; do
+
+cd /go/blog-service/blog/src/cmd && dlv debug -l ":2345" --continue --api-version 2 --headless --log --accept-multiclient --output "/tmp/__blog_debug_bin" --build-flags "-buildvcs=false" &
+
+PID=$!
+
+inotifywait -e modify -e create -e delete -e move -e attrib -r --exclude "\.(css|js|html|git)" /go/blog-service/blog
+
+kill -9 $PID
+pkill -f "/tmp/__blog_debug_bin"
+done
